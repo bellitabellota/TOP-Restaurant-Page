efe4e377000a4e064a5b073000657873c6949280
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-05 16:45:03 +0000

    Refactor the dynamic html generation for 'Home' into a assembleHompage function

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -16,22 +16,49 @@ dishOfMonthImageElement.className ="dish-of-month-image";
 export function pageLoad() {
   const content = document.getElementById("content");
 
-  content.innerHTML=`
-      <h1>Peaceful Dining</h1>
-      <p>True enjoyment is a holistic experience. Not only do we prepare fresh and healthy dishes from farm-to-table ingredients, we have also created an ambience that stimulates the senses so that you can leave the hustle and bustle behind and focus on the taste experience.</p>
-      <h2>How To Find Us</h2>
-      <div class="stack">
-        <p>In a remote, utopian location in the heart of the Valley of a Thousand Rivers:
-        </p>
-        <div class="small-stack address">
-          <p>Riverstreet 5</p>
-          <p>9999 New South Riverland</p>
-        </div>
-      </div>
-      
-      <h2>February's Most Popular Dish</h2>
-  `;
-  
-  content.insertBefore(restaurantImageElement, content.firstChild);
+  assembleHomepage(content);
+}
+
+function assembleHomepage(content) {
+  const h1 = document.createElement("h1");
+  h1.textContent = "Peaceful Dining";
+
+  const paragraph1 = createParagraph("True enjoyment is a holistic experience. Not only do we prepare fresh and healthy dishes from farm-to-table ingredients, we have also created an ambience that stimulates the senses so that you can leave the hustle and bustle behind and focus on the taste experience.");
+  const firstH2 = createHeading2("How To Find Us");
+  const stackDiv = createDivWithClass("stack");
+  const paragraph2 = createParagraph("In a remote, utopian location in the heart of the Valley of a Thousand Rivers:");
+  const addressDiv = createDivWithClass("small-stack address");
+  const paragraph3 = createParagraph("Riverstreet 5");
+  const paragraph4 = createParagraph("9999 New South Riverland");
+  const heading3 = createHeading2("February's Most Popular Dish");
+
+  content.appendChild(restaurantImageElement);
+  content.appendChild(h1);
+  content.appendChild(paragraph1);
+  content.appendChild(firstH2);
+  content.appendChild(stackDiv);
+  stackDiv.appendChild(paragraph2);
+  stackDiv.appendChild(addressDiv);
+  addressDiv.appendChild(paragraph3);
+  addressDiv.appendChild(paragraph4);
+  content.appendChild(heading3);
   content.appendChild(dishOfMonthImageElement);
+}
+
+function createParagraph(text){
+  const p = document.createElement("p");
+  p.textContent = text;
+  return p;
+}
+
+function createHeading2(text){
+  const h2 = document.createElement("h2");
+  h2.textContent = text;
+  return h2;
+}
+
+function createDivWithClass(className){
+  const div = document.createElement("div");
+  div.className = className;
+  return div;
 }
