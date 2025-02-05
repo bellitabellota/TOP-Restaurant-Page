4e8adbea9e3211262c14e85f73220c31bb9914c4
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-05 17:36:10 +0000

    Make a createImg and createHeading1 function

--- src/modules/assemble-hompage.js
+++ src/modules/assemble-hompage.js
@@ -1,21 +1,11 @@
 import restaurantImage from "../images/josh-berendes-WItC-f-K2Ho-unsplash.jpg";
 import dishOfMonthImage from "..//images/farhad-ibrahimzade-59lfMHMZugY-unsplash.jpg";
-import { createParagraph, createDivWithClass, createHeading2 } from "./creating-html-elements";
+import { createParagraph, createDivWithClass, createHeading2, createHeading1, createImg } from "./creating-html-elements";
 
-const restaurantImageElement = document.createElement("img");
-restaurantImageElement.src = restaurantImage;
-restaurantImageElement.alt = "Our Restaurant in the Valley of a Thousand Rivers";
-restaurantImageElement.className = "restaurant-image";
-
-const dishOfMonthImageElement = document.createElement("img");
-dishOfMonthImageElement.src = dishOfMonthImage;
-dishOfMonthImageElement.alt = "Summer Salad";
-dishOfMonthImageElement.className ="dish-of-month-image";
 
 export function assembleHomepage(content) {
-  const h1 = document.createElement("h1");
-  h1.textContent = "Peaceful Dining";
-
+  const restaurantImageElement = createImg(restaurantImage, "Our Restaurant in the Valley of a Thousand Rivers", "restaurant-image");
+  const h1 = createHeading1("Peaceful Dining");
   const paragraph1 = createParagraph("True enjoyment is a holistic experience. Not only do we prepare fresh and healthy dishes from farm-to-table ingredients, we have also created an ambience that stimulates the senses so that you can leave the hustle and bustle behind and focus on the taste experience.");
   const firstH2 = createHeading2("How To Find Us");
   const stackDiv = createDivWithClass("stack");
@@ -24,6 +14,7 @@ export function assembleHomepage(content) {
   const paragraph3 = createParagraph("Riverstreet 5");
   const paragraph4 = createParagraph("9999 New South Riverland");
   const heading3 = createHeading2("February's Most Popular Dish");
+  const dishOfMonthImageElement = createImg(dishOfMonthImage, "Summer Salad", "dish-of-month-image");
 
   content.appendChild(restaurantImageElement);
   content.appendChild(h1);
--- src/modules/creating-html-elements.js
+++ src/modules/creating-html-elements.js
@@ -4,6 +4,12 @@ export function createParagraph(text){
   return p;
 }
 
+export function createHeading1(text){
+  const h1 = document.createElement("h1");
+  h1.textContent = text;
+  return h1;
+}
+
 export function createHeading2(text){
   const h2 = document.createElement("h2");
   h2.textContent = text;
@@ -14,4 +20,12 @@ export function createDivWithClass(className){
   const div = document.createElement("div");
   div.className = className;
   return div;
+}
+
+export function createImg(src, alt, className){
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  img.className = className;
+  return img;
 }
